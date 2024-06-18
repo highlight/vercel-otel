68b7e556069a794335523348da9d5ebff327f6fb
Chris Esplin <chris@chrisesplin.com>
2024-06-18 13:09:23 -0600

    Logging cookies

--- src/app/init-highlight-server.ts
+++ src/app/init-highlight-server.ts
@@ -7,8 +7,11 @@ export async function initHighlightServer() {
     const tracer = trace.getTracer('vercel-otel-tracer');
     const span = tracer.startSpan('initHighlightServer');
 
-    const sessionSecureID = cookies().get('sessionSecureID');
+    const requestCookies = cookies();
+    const sessionSecureID = requestCookies.get('sessionSecureID');
     const activeSpan = trace.getSpan(context.active());
+
+    console.log('All cookies:', requestCookies.getAll());
 
     activeSpan?.setAttribute('highlight.session_id', sessionSecureID?.value || '');
 
